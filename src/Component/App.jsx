df27adf987ba76a943c5d0a148a334c8bbf8edb3
arunkumar2011 <akumar6@rh.com>
2022-11-23 17:45:25 +0530

    todo

--- src/Component/App.jsx
+++ src/Component/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as BR, Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import '../asset/css/style.css'
 export default function App() {
     return (
         <BR>
