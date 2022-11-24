10ea6b17249f4d97d90e594f67c12e4ec4cb72dc
arunkumar2011 <akumar6@rh.com>
2022-11-24 14:15:53 +0530

    Step 1 Release

--- src/Component/App.jsx
+++ src/Component/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as BR, Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import Navbar from './Navbar'
 import '../asset/css/style.css'
 export default function App() {
     return (
         <BR>
+         <Navbar/>
             <Routes>
                 <Route path='/' element={<Home />} />
             </Routes>
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 export default function Home() {
-  return (
-    <div>
-      <h1>hi</h1>
-    </div>
-  )
+    const [data, setdata] = useState([{
+        "task": "abc",
+        "status": "pending"
+    },
+    {
+        "task": "xyz",
+        "status": "done"
+    }
+
+    ])
+    useEffect(() => {
+
+    }, [data])
+    return (
+        <div>
+            <div className="container mt-2">
+                <h2>Todo List</h2>
+                <ul className="list-group w-100 mt-2">
+                    {
+                        data.map((item, i) => {
+                            return <li key={i} className="list-group-item d-flex justify-content-between align-items-start">
+                                <div className="ms-2 me-auto">
+                                    <input className="form-check-input me-1" type="checkbox" value="" id={`${i}CheckboxStretched`} defaultChecked={item.status === "done" ? true : false} />
+                                    <label className="form-check-label stretched-link" for={`${i}CheckboxStretched`}>{item.task}</label>
+                                </div>
+                                {item.status === "done" ? <span className="badge bg-secondary">Completed</span> : ""}
+                            </li>
+                        })
+                    }
+                </ul>
+            </div>
+        </div>
+    )
 }
