99905d2e7d6c9338d724a55d6125ec9fcfa8147a
arunkumar2011 <akumar6@rh.com>
2022-11-24 19:11:02 +0530

    Step 3 Release updated

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,18 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 export default function Home() {
-    const [data, setdata] = useState([{
-        "task": "abc",
-        "status": "pending"
-    },
-    {
-        "task": "xyz",
-        "status": "done"
-    }
-
-    ])
-    const [todo, settodo] = useState("")
-
-
+    let todolist = []
+    const [data, setData] = useState(todolist)
+    const [todo, setTodo] = useState("")
+    const [editable, setEditable] = useState(-1)
+    const [editvalue, setEditvalue] = useState("")
     const PostData = (e) => {
         const box = document.getElementById('todoinput')
         e.preventDefault()
@@ -22,18 +14,31 @@ export default function Home() {
                 "task": todo,
                 "status": "pending"
             }]
-            setdata(newdata)
-            settodo("")
+            setData(newdata)
+            setTodo("")
         }
         else {
             box.classList.add('input-validate')
         }
     }
-
-
-
-    useEffect(() => {
-    }, [data])
+    const Marked = (i) => {
+        let newtodo = [...data]
+        newtodo[i].status = newtodo[i].status === "done" ? "pending" : "done"
+        setData(newtodo)
+    }
+    const DeleteTodo = (i) => {
+        console.log(i);
+        let newtodo = [...data]
+        newtodo = newtodo.filter((todo, index) => index !== i)
+        setData(newtodo)
+    }
+    const EditTodo = (e, i) => {
+        e.preventDefault()
+        let newtodo = [...data]
+        newtodo[i].task = editvalue === "" ? newtodo[i].task : editvalue
+        setData(newtodo)
+        setEditable(-1)
+    }
     return (
         <div>
             <div className="container-fluid mt-2">
@@ -41,20 +46,27 @@ export default function Home() {
                 <ul className="list-group w-100 mt-2">
                     {
                         data.map((item, i) => {
-                            return <li key={i} className="list-group-item d-flex justify-content-between align-items-start">
-                                <div className="ms-2 me-auto">
-                                    <input className="form-check-input me-1" type="checkbox" value="" id={`${i}CheckboxStretched`} defaultChecked={item.status === "done" ? true : false} />
-                                    <label className="form-check-label stretched-link" for={`${i}CheckboxStretched`}>{item.task}</label>
+                            return <div key={i} className="li-div">
+                                <li className="list-group-item d-flex justify-content-between align-items-start">
+                                    <div className="ms-2 me-auto">
+                                        {editable !== i ? <div><input className="form-check-input me-1" type="checkbox" value="" id={`${i}CheckboxStretched`} defaultChecked={item.status === "done" ? true : false} onChange={() => Marked(i)} />
+                                            <label className="form-check-label stretched-link" for={`${i}CheckboxStretched`}>{item.task}</label></div> : ""}
+                                    </div>
+                                    {editable === i ? <form className='w-100' onSubmit={(e) => EditTodo(e, i)}> <input type="text" className="form-control" defaultValue={item.task} required onChange={(E) => setEditvalue(E.target.value)} /></form> : ""}
+                                    {item.status === "done" ? <span className="badge bg-secondary mx-3">Completed</span> : ""}
+                                </li>
+                                <div className="d-flex justify-content-around">
+                                    <button className="btn btn-sm btn-danger mt-2 mb-2" onClick={() => DeleteTodo(i)}><i className="fa-sharp fa-solid fa-trash"></i></button>
+                                    <button className="btn btn-sm btn-danger mt-2 mb-2" onClick={() => setEditable(i)}><i className="fa-solid fa-pen"></i></button>
                                 </div>
-                                {item.status === "done" ? <span className="badge bg-secondary">Completed</span> : ""}
-                            </li>
+                            </div>
                         })
                     }
                 </ul>
                 <form className='mt-2' onSubmit={PostData}>
                     <div className="mb-3">
                         <label className="form-label">Todo</label>
-                        <input type="text" id='todoinput' className="form-control" placeholder="Create your Todo" onChange={(e) => settodo(e.target.value)} value={todo} />
+                        <input type="text" id='todoinput' className="form-control" placeholder="Create your Todo" onChange={(e) => setTodo(e.target.value)} value={todo} />
                     </div>
                     <button type='sumbit' className='btn btn-primary'>Submit</button>
                 </form>
